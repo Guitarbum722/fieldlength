22663302e60e90077c7139c1aa902fa45893ffcb
John Moore <johnkenneth.moore@gmail.com>
2017-12-08 06:49:56 -0700

    test Split

--- fields_test.go
+++ fields_test.go
@@ -1,1 +1,43 @@
 package qualified
+
+import "testing"
+
+var splitCases = []struct {
+	input, sep, qual string
+	expected         int // num of resulting elements
+}{
+	{
+		"one,two,three,four",
+		",",
+		"",
+		4,
+	},
+	{
+		"\"one,two\",three,four",
+		",",
+		"\"",
+		3,
+	},
+	{
+		"one",
+		",",
+		"",
+		1,
+	},
+	{
+		"one|two|three|four",
+		"|",
+		"",
+		4,
+	},
+}
+
+func TestSplitWithQual(t *testing.T) {
+	for _, tt := range splitCases {
+		got := SplitWithQual(tt.input, tt.sep, tt.qual)
+
+		if len(got) != tt.expected {
+			t.Fatalf("SplitWithQual(%v) = len(%v) ; want %v", tt.input, len(got), tt.expected)
+		}
+	}
+}
